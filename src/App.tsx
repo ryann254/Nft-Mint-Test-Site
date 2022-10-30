9d4cea84a84e5dee02bf401ca7c9b7ef9da41cf7
ryann254 <keddelyronjoz@gmail.com>
2022-10-30 09:09:33 +0300

    feat: connect to metamask wallet

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
+import { ReactElement, useEffect, useState } from 'react';
+
 import './styles/App.css';
 import twitterLogo from './assets/twitter-logo.svg';
-import React from "react";
 
 // Constants
 const TWITTER_HANDLE = '_buildspace';
@@ -8,36 +9,95 @@ const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 const OPENSEA_LINK = '';
 const TOTAL_MINT_COUNT = 50;
 
-const App = () => {
-  // Render Methods
-  const renderNotConnectedContainer = () => (
-    <button className="cta-button connect-wallet-button">
-      Connect to Wallet
-    </button>
-  );
+const App = (): ReactElement => {
+	const [currentAccount, setcurrentAccount] = useState('');
 
-  return (
-    <div className="App">
-      <div className="container">
-        <div className="header-container">
-          <p className="header gradient-text">My NFT Collection</p>
-          <p className="sub-text">
-            Each unique. Each beautiful. Discover your NFT today.
-          </p>
-          {renderNotConnectedContainer()}
-        </div>
-        <div className="footer-container">
-          <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
-          <a
-            className="footer-text"
-            href={TWITTER_LINK}
-            target="_blank"
-            rel="noreferrer"
-          >{`built on @${TWITTER_HANDLE}`}</a>
-        </div>
-      </div>
-    </div>
-  );
+	const checkIfWalletIsConnected = async (): Promise<void> => {
+		//@ts-ignore
+		const { ethereum } = window;
+
+		if (!ethereum) {
+			alert('Make sure you have metamask!');
+		} else {
+			console.log('We have the ethereum object', ethereum);
+		}
+
+		const accounts = await ethereum.request({ method: 'eth_accounts' });
+
+		if (accounts.length !== 0) {
+			const account = accounts[0];
+			console.log('Found an authorized account: ', account);
+			setcurrentAccount(account);
+		} else {
+			console.log('No authorized account found');
+		}
+	};
+
+	const connectWallet = async () => {
+		try {
+			//@ts-ignore
+			const { ethereum } = window;
+
+			if (!ethereum) {
+				alert('Get Metamask!');
+			}
+
+			const accounts = await ethereum.request({
+				method: 'eth_requestAccounts',
+			});
+
+			console.log('Connected', accounts[0]);
+			setcurrentAccount(accounts[0]);
+		} catch (error) {
+			console.log('Error: ', error);
+		}
+	};
+
+	// Render Methods
+	const renderNotConnectedContainer = () => (
+		<button
+			onClick={connectWallet}
+			className='cta-button connect-wallet-button'
+		>
+			Connect to Wallet
+		</button>
+	);
+
+	useEffect(() => {
+		checkIfWalletIsConnected();
+	}, []);
+
+	return (
+		<div className='App'>
+			<div className='container'>
+				<div className='header-container'>
+					<p className='header gradient-text'>My NFT Collection</p>
+					<p className='sub-text'>
+						Each unique. Each beautiful. Discover your NFT today.
+					</p>
+					{currentAccount === '' ? (
+						renderNotConnectedContainer()
+					) : (
+						<button
+							onClick={() => {}}
+							className='cta-button connect-wallet-button'
+						>
+							Mint NFT
+						</button>
+					)}
+				</div>
+				<div className='footer-container'>
+					<img alt='Twitter Logo' className='twitter-logo' src={twitterLogo} />
+					<a
+						className='footer-text'
+						href={TWITTER_LINK}
+						target='_blank'
+						rel='noreferrer'
+					>{`built on @${TWITTER_HANDLE}`}</a>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default App;
